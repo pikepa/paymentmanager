3ff2407fb1ff97175610f974f0f8cb1353f75ba1
Peter Pike <pikepeter@gmail.com>
2025-01-01 08:01:05 +1000

    initial commit

---
+++ src/PaymentManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace pikepa\PaymentManager;
+
+class PaymentManager
+{
+    protected $gateway;
+
+    public function __construct($gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function processPayment($amount, $currency)
+    {
+        // Simulate processing payment through the selected gateway
+        return "Processing $amount $currency via " . $this->gateway;
+    }
+}
